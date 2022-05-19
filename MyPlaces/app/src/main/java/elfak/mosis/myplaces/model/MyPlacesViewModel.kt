db2f5351294351eec4d035d35eae0423dc4d8994
17431ZeljkoStanojevic <zeljko.stanojevic@elfak.rs>
2022-05-19 15:48:17 +0200

    Treca lab vezba

---
+++ MyPlaces/app/src/main/java/elfak/mosis/myplaces/model/MyPlacesViewModel.kt
@@ -0,0 +1,11 @@
+package elfak.mosis.myplaces.model
+
+import androidx.lifecycle.ViewModel
+import elfak.mosis.myplaces.data.MyPlace
+
+class MyPlacesViewModel: ViewModel() {
+    var myPlacesList: ArrayList<MyPlace> = ArrayList<MyPlace>()
+    fun addPlace(place: MyPlace) {
+        myPlacesList.add(place)
+    }
+}
